5768e76d802147688a181ef1acec1f82a4e1c917
Himanshu Pradhan <83631213+itmaybehimm@users.noreply.github.com>
2025-03-18 01:12:32 +0545

    Update Hero.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -26,7 +26,7 @@ const Hero = () => {
   return (
     <section className="min-h-screen w-full flex flex-col relative" id="home">
       <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
-        <p className="hero_tag text-gray_gradient">Network Intrusion </p>
+        <p className="hero_tag text-gray_gradient">Network Intrusion Detection</p>
       </div>
 
       <div className="w-full h-full absolute inset-0">
